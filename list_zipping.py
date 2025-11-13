b9d83dd15d34631f89ef64b84d19a129a2086706
Adama Bagayoko <adamsbag84@gmail.com>
2025-11-12 20:29:17 -0800

    Explanation on List Zipping

--- list_zipping.py
+++ list_zipping.py
@@ -4,7 +4,7 @@ ages = [23, 45, 39]
 zipped = zip(names,ages) # creates a tupple of names and ages
 print(list(zipped)) # Print [('Tim', 23), ('Joe', 45), ('Adama', 39)]
 
-
+#print [('Tim", 23)]
 
 
 
